df1cc03be14fd178baa8e27f9a7179b79df1192c
Alona <alona.costa2@gmail.com>
2024-12-18 11:35:34 +0000

    Completed Leave Management Module

--- LeaveManagementSystem.Web/Controllers/HomeController.cs
+++ LeaveManagementSystem.Web/Controllers/HomeController.cs
@@ -25,8 +25,6 @@ namespace LeaveManagementSystem.Web.Controllers
             return View();
         }
 
-    
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
